54080878742b92207cc3671803a14edba52f2a81
AquaUseful <l.vladik.m@gmail.com>
2022-11-02 00:14:53 +0300

    Add division hash counting

--- form/include/LabForm.hpp
+++ form/include/LabForm.hpp
@@ -1,18 +1,28 @@
 #pragma once
 
 #include "ui_form.h"
-#include <qobjectdefs.h>
+#include <cstddef>
+#include <cstdint>
+
+#include "hash.hpp"
 
 class LabForm : public QMainWindow {
 
   Q_OBJECT
 
 public:
-  explicit LabForm(QWidget *parent = nullptr);
+  explicit LabForm(QWidget* parent = nullptr);
   virtual ~LabForm();
 
 private:
   void configure_slots();
+  void update_division_hash();
 
-  Ui::LabForm *ui;
+private slots:
+  void update_hashes();
+
+private:
+  Ui::LabForm* ui;
+  const std::size_t hash_key {45678};
+  const std::size_t index_count {1000};
 };
--- form/src/LabForm.cpp
+++ form/src/LabForm.cpp
@@ -1,15 +1,29 @@
 #include "LabForm.hpp"
 
+#include <string>
 
-LabForm::LabForm(QWidget *parent) : QMainWindow(parent), ui{new Ui::LabForm} {
+LabForm::LabForm(QWidget* parent) : QMainWindow(parent), ui {new Ui::LabForm} {
   ui->setupUi(this);
   ui->retranslateUi(this);
 
   configure_slots();
 }
 
-LabForm::~LabForm() { delete ui; }
+LabForm::~LabForm() {
+  delete ui;
+}
 
 void LabForm::configure_slots() {
   connect(ui->quitBtn, SIGNAL(clicked()), this, SLOT(close()));
+  connect(ui->calculateBtn, SIGNAL(clicked()), this, SLOT(update_hashes()));
+}
+
+void LabForm::update_hashes() {
+  update_division_hash();
+}
+
+void LabForm::update_division_hash() {
+  const auto hash = hash_division(hash_key, std::size_t(997));
+  const auto hash_str = std::to_string(hash);
+  ui->divHash->setText(QString::fromStdString(hash_str));
 }
