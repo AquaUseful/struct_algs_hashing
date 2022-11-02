22e9c1e892aa5ceb1e30b6f651049004a3615845
AquaUseful <l.vladik.m@gmail.com>
2022-11-02 10:08:57 +0300

    Avoid shortening type casting

--- hashing/include/hash_division.hpp
+++ hashing/include/hash_division.hpp
@@ -3,6 +3,7 @@
 #include <concepts>
 #include <cstddef>
 
-template <std::unsigned_integral T> inline constexpr auto hash_division(const T key, const T divisor) -> std::size_t {
+template <std::unsigned_integral KeyT, std::unsigned_integral DivT>
+inline constexpr auto hash_division(const KeyT key, const DivT divisor) -> std::size_t {
   return key % divisor;
 }
