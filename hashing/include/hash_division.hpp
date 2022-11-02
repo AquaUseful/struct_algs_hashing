efe48a2705ebdf045a0c432ea8f003c65d98f0ef
AquaUseful <l.vladik.m@gmail.com>
2022-11-02 09:52:33 +0300

    Formatting

--- hashing/include/hash_division.hpp
+++ hashing/include/hash_division.hpp
@@ -3,8 +3,6 @@
 #include <concepts>
 #include <cstddef>
 
-template <std::unsigned_integral T>
-inline constexpr auto hash_division(const T key, const T divisor)
-    -> std::size_t {
+template <std::unsigned_integral T> inline constexpr auto hash_division(const T key, const T divisor) -> std::size_t {
   return key % divisor;
 }
