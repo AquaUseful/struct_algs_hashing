ec0c50ed9b9299f016793e0273888804138f24e5
AquaUseful <l.vladik.m@gmail.com>
2022-11-02 10:09:15 +0300

    Remove unnened requirements

--- hashing/include/hash_multiplication.hpp
+++ hashing/include/hash_multiplication.hpp
@@ -5,7 +5,6 @@
 #include <cstddef>
 
 template <std::unsigned_integral KeyT, std::floating_point CoefT>
-requires std::convertible_to<KeyT, CoefT>
 inline constexpr auto hash_multiplication(const KeyT key, const CoefT coef, const std::size_t index_count) -> std::size_t {
   assert((coef > 0) && (coef < 1));
   const auto mkey = key * coef;
