9aad64699bddfedabd3d62f344222c145717b211
AquaUseful <l.vladik.m@gmail.com>
2022-11-09 12:22:11 +0300

    fix

--- hashing/include/hash_square_center.hpp
+++ hashing/include/hash_square_center.hpp
@@ -9,9 +9,13 @@
 inline constexpr std::uint64_t hash_midsquare(std::uint64_t key,
                                               std::uint64_t index_digits) {
   std::uint64_t square = key * key;
-  std::uint64_t square_length = std::trunc(std::log10(square));
+  std::uint64_t square_length = std::trunc(std::log10(square) + 1);
   std::uint64_t div = 1;
   for (auto i = 0; i < index_digits; div *= 10, ++i)
     ;
-  return (square / ((square_length - index_digits) / 2)) % div;
+  std::uint64_t trunc = 1;
+  for (auto i = 0; i < ((square_length - index_digits) / 2); trunc *= 10, ++i)
+    ;
+
+  return ((square / trunc) % div);
 }
